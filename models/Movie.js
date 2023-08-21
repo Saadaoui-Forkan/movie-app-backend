8837b324f76979d250d09b2a9b7a333f2a58383e
unknown <forkan.saadaoui@gmail.com>
2023-08-21 17:57:01 +0100

    UserModel & MovieModel

---
+++ models/Movie.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const ModelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rate: {
+        type: Number,
+        default: 0
+    },
+    reviews: {
+        type: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User'
+                },
+                comment: String,
+                rate: Number
+            }
+        ],
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+ModelSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,  // hidden _v in database
+    transform: (doc, ret) => {
+        delete ret._id  // delete _id in db
+    }
+})
+
+const Model = mongoose.model('Movie', ModelSchema)
+
+module.exports = Model
