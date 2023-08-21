22f81649ba6dc914a1b11c4cbf889783fb621779
unknown <forkan.saadaoui@gmail.com>
2023-08-21 17:52:11 +0100

    route files

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 
 app.get('/', (req,res) => res.send('API running'))
 
+app.use('/api/users', require('./api/users'))
+app.use('/api/posts', require('./api/movies'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
