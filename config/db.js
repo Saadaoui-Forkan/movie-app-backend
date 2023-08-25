ddf3a230e78a9be9aa0a95b8af99bb77312dfbe5
unknown <forkan.saadaoui@gmail.com>
2023-08-25 11:29:32 +0100

    fixing error in connection to database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
+
 const db = process.env.mongoURI
 
 const connectDB = async () => {
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express()
+
+// connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json())
 
 app.get('/', (req,res) => res.send('API running'))
 
