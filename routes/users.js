b487b31a3358687cbbd4751fc80a2ec6e7599a38
unknown <forkan.saadaoui@gmail.com>
2023-08-25 22:18:28 +0100

    Login

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const jwtHeelpers = require('../utils/jwtHelpers')
 
-// @route   POST api/users
+// @route   POST api/users/register
 // @desc    Register new user
 // @access  Public
-router.post('/', [
+router.post('/register', [
         check('name', 'Name is required with 30 or less characters').not().isEmpty().isLength({max: 30}),
         check('email', 'Please include a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({min: 6}),
@@ -36,6 +37,44 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt)
         await user.save()
         res.send(' user created ')
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server error')
+    }
+})
+
+// @route   POST api/users/login
+// @desc    User Login
+// @access  Public
+router.post('/login', [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6}),
+    ] , async(req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const { email, password } = req.body
+
+    try {
+        
+        let user = await User.findOne({ email })
+        
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.json({
+                success: true,
+                data: {
+                    id: user.id,
+                    name: user.name,
+                    accessToken: jwtHeelpers.sign({ sub: user.id })
+                }
+            })
+        } else {
+            return res.status(400).json({errors: [{msg: 'User already exists'}]})
+
+        }
 
     } catch (err) {
         console.log(err.message)
